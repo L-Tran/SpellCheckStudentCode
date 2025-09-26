ddb2b80bf0b224eeafe21281efcb3297c9b20508
L-Tran <logan.tran@menloschool.org>
2025-09-26 09:10:44 -0700

    Finished Binary search

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -20,32 +20,38 @@ public class SpellCheck {
      * @return String[] of all mispelled words in the order they appear in text. No duplicates.
      */
     public String[] checkWords(String[] text, String[] dictionary) {
-
-
         // Loop through text
         ArrayList<String> misspelled = new ArrayList<>();
-        int low = 0;
-        int high = dictionary.length - 1;
         for(String word: text) {
+            int low = 0;
+            int high = dictionary.length - 1;
+            boolean found = false;
             while (low <= high) {
                 int mid = low + (high - low) / 2;
-                if (word.equals(dictionary[mid])) {
+                int compare = dictionary[mid].compareTo(word);
+                if (compare == 0) {
+                    found = true;
                     break;
                 }
 
-                else if(word < dictionary[mid]) {
+                else if(compare < 0) {
                     low = mid + 1;
                 }
                 else {
                     high = mid - 1;
                 }
             }
-            misspelled.add(word);
+            if(!found && !misspelled.contains(word)) {
+                misspelled.add(word);
+            }
         }
-        String[] misspelledWords =
-        for(String s: misspelled) {
 
+        String[] misspelledWords = new String[misspelled.size()];
+        for(int i = 0; i < misspelledWords.length; i++) {
+            misspelledWords[i] = misspelled.get(i);
         }
-        return null;
+
+
+        return misspelledWords;
     }
 }
